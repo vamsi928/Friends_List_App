c3cb5c7477fe5f814a4cd836d1c3ea3e7335eb4b
vamsi928 <45943665+vamsi928@users.noreply.github.com>
2021-08-01 22:08:37 +0530

    Update store.js

--- src/state/store.js
+++ src/state/store.js
@@ -8,6 +8,6 @@ if (process.env.NODE_ENV === "development") {
   middlewares.push(logger);
 }
 
-const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 export default store;
